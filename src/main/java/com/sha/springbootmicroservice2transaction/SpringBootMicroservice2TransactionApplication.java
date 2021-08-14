ef630683d75b67608edb4985464042432610eb96
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-14 18:41:25 +0300

    project created

---
+++ src/main/java/com/sha/springbootmicroservice2transaction/SpringBootMicroservice2TransactionApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootmicroservice2transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice2TransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice2TransactionApplication.class, args);
+	}
+
+}
