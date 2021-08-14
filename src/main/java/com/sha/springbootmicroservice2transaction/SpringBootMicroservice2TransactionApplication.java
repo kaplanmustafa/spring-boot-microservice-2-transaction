8a4c762d007aa74bb0dcc16a80db7cedc9852fb6
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-14 22:07:40 +0300

    configuration profiles

--- src/main/java/com/sha/springbootmicroservice2transaction/SpringBootMicroservice2TransactionApplication.java
+++ src/main/java/com/sha/springbootmicroservice2transaction/SpringBootMicroservice2TransactionApplication.java
@@ -2,8 +2,10 @@ package com.sha.springbootmicroservice2transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroservice2TransactionApplication {
 
 	public static void main(String[] args) {
