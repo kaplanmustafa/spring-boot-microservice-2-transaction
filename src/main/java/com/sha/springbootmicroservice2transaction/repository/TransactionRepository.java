e3971731f9476240b2e7e5a76be1bf96c0f95896
INNOVA\muskaplan <muskaplan@innova.com.tr>
2021-08-14 19:10:54 +0300

    transaction repository with Spring Data JPA

---
+++ src/main/java/com/sha/springbootmicroservice2transaction/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.sha.springbootmicroservice2transaction.repository;
+
+import com.sha.springbootmicroservice2transaction.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findAllByUserId(Long userId);
+}
